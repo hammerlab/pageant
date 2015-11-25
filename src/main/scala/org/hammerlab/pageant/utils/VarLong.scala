2d366736b97f0403459118afc93f97b7c1090a5b
Ryan Williams <ryan.blake.williams@gmail.com>
2015-11-25 05:01:29 +0000

    fix/impl varlong

--- src/main/scala/org/hammerlab/pageant/utils/VarLong.scala
+++ src/main/scala/org/hammerlab/pageant/utils/VarLong.scala
@@ -1,10 +1,9 @@
 package org.hammerlab.pageant.utils
 
-import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.{Input, Output}
 
 object VarLong {
-  def write(kryo: Kryo, output: Output, l: Long): Unit = {
+  def write(output: Output, l: Long): Unit = {
     var n = l
     var more = true
     var total = 0
@@ -15,22 +14,23 @@ object VarLong {
       } else {
         val b = (n & 0x7F).toByte
         n = n >> 7
+        total += 7
         more = (n > 0)
         output.writeByte(b | (if (more) 0x80 else 0).toByte)
       }
     }
   }
 
-  def read(kryo: Kryo, input: Input): Long = {
+  def read(input: Input): Long = {
     var l = 0L
     var bytes = 0
     while (bytes < 64) {
       val b = input.readByte()
       if (bytes == 56) {
-        l = (l << bytes) + b
+        l += (b.toLong << bytes)
         bytes += 8
       } else {
-        l = (l << bytes) + (b & 0x7F)
+        l += (b & 0x7F).toLong << bytes
         if ((b & 0x80) == 0) {
           bytes = 64
         } else {
