455d4f44ce1e3e87aefac48e364a7d6323f45863
Ryan Williams <ryan.blake.williams@gmail.com>
2016-02-02 22:24:45 +0000

    fm WIP

--- src/main/scala/org/hammerlab/pageant/suffixes/SparkFM.scala
+++ src/main/scala/org/hammerlab/pageant/suffixes/SparkFM.scala
@@ -1,12 +1,8 @@
 package org.hammerlab.pageant.suffixes
 
-import org.apache.spark.Partitioner
-import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
-import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
-//import PDC3.T
 import SparkFM.{T, V, Idx, PartitionIdx}
 
 case class PartialSum(a: Array[Long], n: Long)
@@ -14,81 +10,74 @@ object PartialSum {
   def apply(a: Array[Long]): PartialSum = PartialSum(a, a.sum)
 }
 
-case class BWTChunk(idx: Long, startCounts: Array[Long], data: Array[T]) {
+case class BWTChunk(startIdx: Long, endIdx: Long, startCounts: Array[Long], data: Array[T]) {
   def occ(t: T, bound: Long): Long = {
-    startCounts(t) + data.take((bound - idx).toInt).count(_ == t)
+    startCounts(t) + data.take((bound - startIdx).toInt).count(_ == t)
+  }
+
+  override def toString: String = {
+    s"BWTC([$startIdx,$endIdx): ${startCounts.mkString(",")}, ${data.mkString(",")}})"
+//    s"BWTChunk($startIdx, $endIdx, Array(${startCounts.mkString(",")}), Array(${data.mkString(",")})"
   }
 }
 
-class SparkFM[U](us: RDD[U], N: Int, countInterval: Int = 100, implicit val toT: (U) => T) {
-  val sc = us.context
-  val count = us.count
-  val t: RDD[T] = us.map(toT)
-  val tZipped: RDD[(Idx, T)] = t.zipWithIndex().map(p => (p._2, p._1))
-  val sa = PDC3(t.map(_.toLong), count)
-  val saZipped: RDD[(Idx, V)] = sa.zipWithIndex().map(p => (p._2, p._1))
-  val uZipped: RDD[(Idx, U)] = us.zipWithIndex().map(p => (p._2, p._1))
+case class Needle(idx: Idx, ts: Array[T], bound: Long, isLow: Boolean) {
+  override def toString: String = {
+    s"Needle($idx, ${ts.mkString(",")}, $bound, $isLow)"
+  }
+}
 
-  val indexedBwtt: RDD[(Idx, T)] =
-    saZipped
+class SparkFM[U](us: RDD[U], N: Int, countInterval: Int = 100, implicit val toT: (U) => T) extends Serializable {
+  @transient val sc = us.context
+  us.cache()
+  val count = us.count
+  @transient val t: RDD[T] = us.map(toT)
+  t.cache()
+  @transient val tZipped: RDD[(Idx, T)] = t.zipWithIndex().map(p => (p._2, p._1))
+  @transient val sa = PDC3(t.map(_.toLong), count)
+  @transient val saZipped: RDD[(V, Idx)] = sa.zipWithIndex()//.map(p => (p._2, p._1))
+  //@transient val uZipped: RDD[(Idx, U)] = us.zipWithIndex().map(p => (p._2, p._1))
+
+  @transient val tShifted: RDD[(Idx, T)] =
+    tZipped
       .map(p =>
         (
-          if (p._1 == count - 1)
+          if (p._1 + 1 == count)
             0L
           else
             p._1 + 1,
           p._2
         )
       )
-      .join(tZipped).map(p => {
-        val (idx, (sufPos, t)) = p
-        (sufPos, t)
-      })
-      .sortByKey()
 
-  val bwtt: RDD[T] = indexedBwtt.map(_._2)
+  @transient val indexedBwtt: RDD[(Idx, T)] =
+    saZipped.join(tShifted).map(p => {
+      val (sufPos, (idx, t)) = p
+      (idx, t)
+    })
+    .sortByKey().setName("indexedBwtt")
 
+  indexedBwtt.cache()
 
-//  val counts = us.mapPartitions(iter => {
-//    val c: mutable.Map[U, Long] = mutable.Map()
-//    iter.foreach(u => c(u) = c.getOrElse(u, 0L) + 1)
-//    c.toIterator
-//  }).reduceByKey(_ + _).collectAsMap()
+  @transient val bwtt: RDD[T] = indexedBwtt.map(_._2).setName("bwtt")
 
-//  indexedBwt.mapPartitionsWithIndex((idx, iter) => {
-//    val c: mutable.Map[U, Long] = mutable.Map()
-//    iter.foreach(u => c(u) = c.getOrElse(u, 0L) + 1)
-//    c.toIterator.map(p => (idx, p))
-//  })
+  @transient val partitionCounts: RDD[Array[Int]] =
+    bwtt.mapPartitions(
+      iter => {
+        var counts: Array[Int] = Array.fill(N)(0)
+        iter.foreach(t => {
+          counts(t) += 1
+        })
+        Array(counts).iterator
+      },
+      preservesPartitioning = false
+    ).setName("partitionCounts")
 
-  val partitionCounts: RDD[Array[Int]] =
-    bwtt.mapPartitions(iter => {
-      var counts: Array[Int] = Array.fill(N)(0)
-      var rets: ArrayBuffer[Array[Int]] = ArrayBuffer()
-      var i = 0
-      iter.foreach(t => {
-        counts(t) += 1
-        if (i == countInterval) {
-          rets.append(counts.clone)
-          i = 0
-        } else {
-          i += 1
-        }
-      })
-      rets.toIterator
-    }).setName("interval counts")
+  @transient val lastCounts: Array[Array[Int]] = partitionCounts.collect
 
-  partitionCounts.cache()
-
-  val lastCounts: Array[Array[Int]] =
-    partitionCounts.mapPartitions(
-      iter => List(iter.toList.last).toIterator
-    ).collect
-
-  val summedCountsBuf: ArrayBuffer[(Array[Long], Long)] = ArrayBuffer()
-  var curSummedCounts = Array.fill(N)(0L)
-  var total = 0L
-//  var partitionIdx = 0
+  @transient val summedCountsBuf: ArrayBuffer[(Array[Long], Long)] = ArrayBuffer()
+  @transient var curSummedCounts = Array.fill(N)(0L)
+  @transient var total = 0L
   lastCounts.foreach(lastCount => {
     summedCountsBuf.append((curSummedCounts.clone(), total))
     var i = 0
@@ -97,80 +86,93 @@ class SparkFM[U](us: RDD[U], N: Int, countInterval: Int = 100, implicit val toT:
       i += 1
       total += c
     })
-//    partitionIdx += 1
   })
-  val totalCounts = curSummedCounts.clone()
-  var totalSums = Array.fill(N)(0L)
-  for {
-    (c, i) <- totalCounts.zipWithIndex
-  } {
-    totalSums(i) = (if (i == 0) 0L else totalSums(i-1)) + c
-  }
 
+  var totalSums = Array.fill(N)(0L)
+  for { i <- 1 until N } {
+    totalSums(i) = totalSums(i-1) + curSummedCounts(i-1)
+  }
   val totalSumsBC = sc.broadcast(totalSums)
 
-  val summedCounts = summedCountsBuf.toArray
-  val summedCountsRDD = sc.parallelize(summedCounts, summedCounts.length)
+  @transient val summedCounts: Array[(Array[Long], Long)] = summedCountsBuf.toArray
+  @transient val summedCountsRDD = sc.parallelize(summedCounts, summedCounts.length)
 
-  val bwtChunks =
+  @transient val bwtChunks =
     indexedBwtt.zipPartitions(summedCountsRDD)((bwtIter, summedCountIter) => {
-      var (counts, total) = summedCountIter.next()
+      var (startCounts, total) = summedCountIter.next()
       assert(
         summedCountIter.isEmpty,
-        s"Got more than one summed-count in partition starting from index ${bwtIter.next()._1}"
+        s"Got more than one summed-count in partition starting from $startCounts $total"
       )
 
       var data: ArrayBuffer[T] = ArrayBuffer()
       var rets: ArrayBuffer[Array[Int]] = ArrayBuffer()
       var chunks: ArrayBuffer[(Long, BWTChunk)] = ArrayBuffer()
       var chunkIdx = -1L
+      var startIdx = -1L
+      var idx = -1L
+      var counts: Array[Long] = null
 
-      for { (idx, t) <- bwtIter } {
+      for {
+        (i, t) <- bwtIter
+      } {
         if (chunkIdx == -1L) {
+          idx = i
           chunkIdx = idx / countInterval
+          startIdx = idx
+          counts = startCounts.clone()
         } else if (idx % countInterval == 0) {
-          chunks.append((chunkIdx, BWTChunk(idx, counts.clone(), data.toArray)))
+          val chunk = BWTChunk(startIdx, idx, startCounts.clone(), data.toArray)
+          println(s"new chunk: $chunk, ${startCounts.mkString(",")}, ${counts.mkString(",")}")
+          chunks.append((chunkIdx, chunk))
           chunkIdx = idx / countInterval
+          startIdx = idx
+          data.clear()
+          startCounts = counts.clone()
         }
         counts(t) += 1
         data.append(t)
+        idx += 1
       }
 
+      if (data.nonEmpty) {
+        chunks.append((chunkIdx, BWTChunk(startIdx, idx, startCounts.clone(), data.toArray)))
+      }
       chunks.toIterator
+    }).groupByKey.mapValues(iter => {
+      val data: ArrayBuffer[T] = ArrayBuffer()
+      val chunks = iter.toArray.sortBy(_.endIdx)
+      val first = chunks.head
+      val last = chunks.last
+      for { chunk <- chunks } { data ++= chunk.data }
+      BWTChunk(first.startIdx, last.endIdx, first.startCounts, data.toArray)
     }).setName("BWTChunks")
 
   bwtChunks.cache()
 
-  //  val partitioner = new Partitioner {
-  //    override def numPartitions: Int = {
-  //      summedCounts.length
-  //    }
-  //
-  //    override def getPartition(key: Any): Int = {
-  //      key.asInstanceOf[Int]
-  //    }
-  //  }
-  //
-  //  val totalCounts: Array[Long] = summedCounts.last._2
-  //  val totalCountsBC: Broadcast[Array[Long]] = sc.broadcast(totalCounts)
-
-  //  def getPartitionIdx(idx: Long): Int = {
-  //    summedCounts.find(_._3 > idx).getOrElse(summedCounts.last)._1
-  //  }
-
-  //def findUs(uss: RDD[Array[U]]): RDD[(Array[T], Long, Long)] = occ(uss.map(_.map(toT)))
   def occ(tss: RDD[Array[T]]): RDD[(Array[T], Long, Long)] = {
-    val tssi = tss.zipWithIndex().map(p => (p._2, p._1))
-    val cur: RDD[(Long, (Idx, Array[T], Long, Boolean))] =
-      tssi.mapPartitions(iter => {
-        for {
-          (tIdx, ts) <- iter
-          bound <- List((0L, true), (count, false))
-        } yield
-          (bound._1 / countInterval, (tIdx, ts, bound._1, bound._2))
-      })
+    val tssi = tss.zipWithIndex().map(p => (p._2, p._1)).setName("tssi")
+    tssi.cache()
+
+    val cur: RDD[(Long, Needle)] =
+      tssi.mapPartitions(
+        iter => {
+          for {
+            (tIdx, ts) <- iter
+            (chunkIdx, bound, isLow) <- List(
+              (0L, 0L, true),                              // Starting low bound
+              ((count - 1) / countInterval, count, false)  // Starting high bound
+            )
+          } yield
+            (
+              chunkIdx,
+              Needle(tIdx, ts, bound, isLow)
+            )
+        }
+      )
 
     val finished = occRec(cur, sc.emptyRDD[(Idx, (Long, Boolean))])
+
     (for {
       (idx, (tsIter, bounds)) <- tssi.cogroup(finished)
     } yield {
@@ -181,8 +183,8 @@ class SparkFM[U](us: RDD[U], N: Int, countInterval: Int = 100, implicit val toT:
       assert(bounds.size == 2, s"Found ${bounds.size} bounds for idx $idx, t $t")
       val (first, second) = (bounds.head, bounds.tail.head)
       val (lo, hi) = (first, second) match {
-        case ((h, true), (l, false)) => (l, h)
-        case ((l, false), (h, true)) => (l, h)
+        case ((l, true), (h, false)) => (l, h)
+        case ((h, false), (l, true)) => (l, h)
         case _ =>
           throw new Exception(
             s"Bad bounds: ${first._1},${first._2} ${second._1},${second._2}"
@@ -193,21 +195,26 @@ class SparkFM[U](us: RDD[U], N: Int, countInterval: Int = 100, implicit val toT:
     }).sortByKey().map(_._2)
   }
 
-  def occRec(cur: RDD[(Long, (Idx, Array[T], Long, Boolean))],
+  def occRec(cur: RDD[(Long, Needle)],
              finished: RDD[(Idx, (Long, Boolean))]): RDD[(Idx, (Long, Boolean))] = {
-    val next: RDD[(Long, (Idx, Array[T], Long, Boolean))] =
+    println(s"recurse:\n\t${cur.collect.mkString("\n\t")}")
+    val next: RDD[(Long, Needle)] =
       cur.cogroup(bwtChunks).flatMap {
         case (chunkIdx, (tuples, chunks)) =>
           assert(chunks.size == 1, s"Got ${chunks.size} chunks for chunk idx $chunkIdx")
           val chunk = chunks.head
           val totalSumsV = totalSumsBC.value
+          println(s"chunk: $chunk")
           for {
-            (idx, ts, bound, isLow) <- tuples
+            Needle(idx, ts, bound, isLow) <- tuples
             lastT = ts.last
             newTs = ts.dropRight(1)
-            newBound = totalSumsV(lastT) + chunk.occ(lastT, bound)
+            c = totalSumsV(lastT)
+            o = chunk.occ(lastT, bound)
+            newBound = c + o
           } yield {
-            (newBound / countInterval, (idx, newTs, newBound, isLow))
+            println(s"${newTs.mkString(",")}($lastT${if (isLow) "↓" else "↑"}): $bound -> $newBound ($c + $o)")
+            (newBound / countInterval, Needle(idx, newTs, newBound, isLow))
           }
       }
 
@@ -215,13 +222,13 @@ class SparkFM[U](us: RDD[U], N: Int, countInterval: Int = 100, implicit val toT:
 
     val newFinished =
       for {
-        (chunkIdx, (idx, ts, bound, isLow)) <- next
+        (chunkIdx, Needle(idx, ts, bound, isLow)) <- next
         if ts.isEmpty
       } yield {
         (idx, (bound, isLow))
       }
 
-    val notFinished = next.filter(_._2._2.nonEmpty).setName("leftover")
+    val notFinished = next.filter(_._2.ts.nonEmpty).setName("leftover")
     notFinished.cache()
     val numLeft = notFinished.count()
     if (numLeft > 0) {
---
+++ src/test/scala/org/hammerlab/pageant/suffixes/SparkFMTest.scala
@@ -0,0 +1,123 @@
+package org.hammerlab.pageant.suffixes
+
+import org.apache.spark.rdd.RDD
+import org.bdgenomics.utils.misc.SparkFunSuite
+import org.scalatest.{BeforeAndAfterAll, Matchers}
+
+object Test {
+  //val ref: Seq[Char] = "CCTGAGGCATGATCCTGACTTCGGGTGCAGGACATTAATTGGTAGCTAATCCAGGCATTAAACGCGGTCGGGAAGTATGAAATCCTGAATGCTCCGTGCC"
+  val toI = "$ACGT".zipWithIndex.toMap
+  val toC = toI.map(p => (p._2, p._1))
+  val ref: Seq[Char] = "ACGTTGCA$"
+}
+
+class SparkFMTest extends SparkFunSuite with Matchers with Serializable {
+
+  import Test.{ref, toC, toI}
+
+  @transient var refRdd: RDD[Char] = _
+  @transient var fm: SparkFM[Char] = _
+
+  sparkTest("simple") {
+
+    println(s"test: $sc")
+    sc.setCheckpointDir("tmp")
+
+    refRdd = sc.parallelize(ref, 2)
+    fm = new SparkFM(refRdd, 5, 2, toI.apply)
+
+    fm.count should be(9)
+
+    fm.sa.getNumPartitions should be(6)
+    val sa = fm.sa.collect
+    sa should be(Array(8, 7, 0, 6, 1, 5, 2, 4, 3))
+
+    fm.tShifted.getNumPartitions should be(2)
+    val tShifted = fm.tShifted.collect
+    tShifted should be(
+      Array(
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 4),
+        (6, 3),
+        (7, 2),
+        (8, 1),
+        (0, 0)
+      )
+    )
+
+    fm.indexedBwtt.getNumPartitions should be(6)
+    val ibwt = fm.indexedBwtt.collect
+    ibwt should be(
+      Array(
+        (0, 1),
+        (1, 2),
+        (2, 0),
+        (3, 3),
+        (4, 1),
+        (5, 4),
+        (6, 2),
+        (7, 4),
+        (8, 3)
+      )
+    )
+
+    fm.bwtt.getNumPartitions should be(6)
+    val bwt = fm.bwtt.collect
+    bwt should be(Array(1, 2, 0, 3, 1, 4, 2, 4, 3))
+
+    bwt.map(toC).mkString("") should be("AC$GATCTG")
+
+    fm.lastCounts should be(
+      Array(
+        Array(0, 1, 1, 0, 0),  // AC
+        Array(1, 0, 0, 0, 0),  // $
+        Array(0, 1, 0, 1, 0),  // GA
+        Array(0, 0, 0, 0, 1),  // T
+        Array(0, 0, 1, 0, 1),  // CT
+        Array(0, 0, 0, 1, 0)   // G
+      )
+    )
+
+    fm.totalSums should be(Array(0L, 1L, 3L, 5L, 7L))
+
+    fm.summedCounts.map(_._2) should be(Array(0, 2, 3, 5, 6, 8))
+    fm.summedCounts.map(_._1) should be(
+      Array(
+        Array(0, 0, 0, 0, 0),
+        Array(0, 1, 1, 0, 0),
+        Array(1, 1, 1, 0, 0),
+        Array(1, 2, 1, 1, 0),
+        Array(1, 2, 1, 1, 1),
+        Array(1, 2, 2, 1, 2)
+      )
+    )
+
+    val chunks = fm.bwtChunks.collect.map(p => (p._1, p._2.toString()))
+    val expected = Array(
+      (0, BWTChunk(0, 2, Array(0,0,0,0,0), Array(1,2))),
+      (1, BWTChunk(2, 4, Array(0,1,1,0,0), Array(0,3))),
+      (2, BWTChunk(4, 6, Array(1,1,1,1,0), Array(1,4))),
+      (3, BWTChunk(6, 8, Array(1,2,1,1,1), Array(2,4))),
+      (4, BWTChunk(8, 9, Array(1,2,2,1,2), Array(3)))
+    ).map(p => (p._1, p._2.toString()))
+
+    chunks should be(expected)
+
+    val needles: List[Array[Int]] = List("A", "C", "G", "T"/*, "CAT"*/).map(_.toArray.map(toI))
+
+    val needlesRdd = sc.parallelize(needles, 2)
+    val actual = fm.occ(needlesRdd).collect.map(p => (p._1.map(toC).mkString(""), p._2, p._3))
+
+    actual should be(
+      Array(
+        ("A", 1, 3),
+        ("C", 3, 5),
+        ("G", 5, 7),
+        ("T", 7, 9)
+      )
+    )
+  }
+}
